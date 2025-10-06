e0fffd3bb24216a1ff7da4bba7669ec39183243c
Tyler Wade <vaxonreal@gmail.com>
2025-10-06 17:26:12 -0400

    Refactor ValidateRolesPage to remove forbiddenRoles handling and simplify role validation logic

--- src/features/auth/components/ValidateRolesPage.tsx
+++ src/features/auth/components/ValidateRolesPage.tsx
@@ -6,10 +6,9 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import LoginBtn from "@/components/LoginBtn";
 
-export default function ValidateRolesPage({children, validRoles, forbiddenRoles = []}: {
+export default function ValidateRolesPage({children, validRoles}: {
   children: React.ReactNode;
   validRoles: string[];
-  forbiddenRoles?: string[];
 }) {
 
   const {status, data: session} = useSession();
@@ -38,7 +37,7 @@ export default function ValidateRolesPage({children, validRoles, forbiddenRoles 
     )
   }
 
-  else if (!session.user.roles || session.user.roles.length === 0 || !validRoles.some(role => session.user.roles.includes(role)) || forbiddenRoles.some(role => session.user.roles.includes(role))) {
+  else if (!session.user.roles || session.user.roles.length === 0 || !validRoles.some(role => session.user.roles.includes(role))) {
     return (
       <div className='pt-24 flex items-center justify-center'>
         <Container maxWidth="xl" className="flex flex-col items-center">
@@ -51,12 +50,6 @@ export default function ValidateRolesPage({children, validRoles, forbiddenRoles 
           <Typography variant="body1" sx={{mb: 2}}>
             Valid Roles: {validRoles.join(", ")}
           </Typography>
-
-          {forbiddenRoles.length > 0 && (
-            <Typography variant="body1" sx={{mb: 2}}>
-              Forbidden Roles: {forbiddenRoles.join(", ")}
-            </Typography>
-          )}
         </Container>
       </div>
     );
